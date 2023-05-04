0172254cded19f29e72e9745e2cce584a8c1c83d
Kateryna Rieznik <rieznik.k@gmail.com>
2023-05-04 21:27:17 +0300

    add platform

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,9 +6,11 @@ export default class Game extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("background", "/public/images/bg_layer1.png");
+    this.load.image("background", "public/images/bg_layer1.png");
+    this.load.image("platform", "public/images/ground_grass.png");
   }
   create() {
     this.add.image(240, 320, "background");
+    this.add.image(240, 320, "platform").setScale(0.5);
   }
 }
