074ce26d4cd9003b9ed3cf5f0e06b18dc9f70b31
Kateryna Rieznik <rieznik.k@gmail.com>
2023-05-05 22:30:06 +0300

    reuse platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 
 export default class Game extends Phaser.Scene {
+  platforms;
+  player;
   constructor() {
     super("game");
   }
@@ -13,23 +15,52 @@ export default class Game extends Phaser.Scene {
   create() {
     this.add.image(240, 320, "background");
 
-    const platforms = this.physics.add.staticGroup();
+    // Create random platforms
+    this.platforms = this.physics.add.staticGroup();
 
     for (let i = 0; i < 5; ++i) {
       const x = Phaser.Math.Between(80, 400);
       const y = 150 * i;
 
-      const platform = platforms.create(x, y, "platform");
+      const platform = this.platforms.create(x, y, "platform");
       platform.scale = 0.5;
 
       const body = platform.body;
       body.updateFromGameObject();
     }
 
-    const player = this.physics.add
+    // Add a player and make it jump when landing
+    this.player = this.physics.add
       .sprite(240, 320, "bunny-stand")
       .setScale(0.5);
 
-    this.physics.add.collider(platforms, player);
+    this.physics.add.collider(this.platforms, this.player);
+
+    this.player.body.checkCollision.up = false;
+    this.player.body.checkCollision.left = false;
+    this.player.body.checkCollision.right = false;
+
+    // follow the player
+    this.cameras.main.startFollow(this.player);
+  }
+
+  update() {
+    // Reuse platforms
+    this.platforms.children.iterate((child) => {
+      const platform = child;
+
+      const scrollY = this.cameras.main.scrollY;
+      if (platform.y >= scrollY + 700) {
+        platform.y = scrollY - Phaser.Math.Between(50, 100);
+        platform.body.updateFromGameObject();
+      }
+    });
+
+    // Jump when landing on the platform
+    const touchingDown = this.player.body.touching.down;
+
+    if (touchingDown) {
+      this.player.setVelocityY(-300);
+    }
   }
 }
